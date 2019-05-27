116535545d82a0bdb727a61f4bc539897e143ce4
Rolf Plagmeijer <sausage948@gmail.com>
2019-05-27 22:26:20 +0200

    Added all key-less dungeons.

--- ootrando_overworldmap_hamsda_triaphlax/scripts/init.lua
+++ ootrando_overworldmap_hamsda_triaphlax/scripts/init.lua
@@ -44,8 +44,8 @@ else
   Tracker:AddLocations("locations/overworld.json")
   Tracker:AddLocations("locations/dung_reg.json")
   Tracker:AddLocations("locations/dung_reg_keys.json")
-  Tracker:AddLocations("locations/dung_mq.json")
-  Tracker:AddLocations("locations/dung_mq_keys.json")
+  --Tracker:AddLocations("locations/dung_mq.json")
+  --Tracker:AddLocations("locations/dung_mq_keys.json")
 
   Tracker:AddLayouts("layouts/item_grids.json")
   Tracker:AddLayouts("layouts/options.json")
